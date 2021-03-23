c813d877fceef48ebf125b82144febe047b84f19
AustenE <austen.elam@gmail.com>
2021-03-23 09:36:00 -0500

    This is a test 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! This is a test");
+            Console.WriteLine("Hello World! This is a test 2");
         }
     }
 }
